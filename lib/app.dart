3a6f3e4f3136a05a57c51066ed77a6135786fb6e
amr <amr@amrs-MBP.home>
2023-08-30 13:34:19 +0300

    Add Ui for booking today

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,9 @@
 import 'package:easycut_business/core/localization/change_local.dart';
 import 'package:easycut_business/core/localization/translation.dart';
 import 'package:easycut_business/helper/binding.dart';
+import 'package:easycut_business/view/screen/home/booking/booking_today.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'routes.dart';
 
 class EasyCutBusiness extends StatelessWidget {
   const EasyCutBusiness({super.key});
@@ -18,8 +17,9 @@ class EasyCutBusiness extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: controller.appTheme,
       initialBinding: MyBinding(),
+      home: BookingToday(),
       // routes: routes,
-      getPages: routes,
+      // getPages: routes,
     );
   }
 }
